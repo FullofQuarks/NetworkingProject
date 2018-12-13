a083bb587dbbf1bb14bdd56ab467762b7c149b0a
Unknown <nicholas.smith4@utdallas.edu>
2018-12-13 02:40:12 -0600

    Router parsing

--- router.cpp
+++ router.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 void readFile();
+void arp(struct host *, int []);
 
 struct network {
     int ip[2];
@@ -29,17 +30,25 @@ int main(int argc, char **argv) {
     {
         networks[ix] = new network;
     }
-    for(int ix = 0; ix < argc-1;)
+    for(int ix = 0; ix < (argc-1)/5; ++ix)
     {
-        networks[ix%5]->ip[0] = strtol(argv[ix+1], NULL, 10);
-        networks[ix%5]->ip[1] = strtol(argv[ix+2], NULL, 10);
-        networks[ix%5]->bid = strtol(argv[ix+3], NULL, 10);
-        networks[ix%5]->port = strtol(argv[ix+4], NULL, 10);
-        networks[ix%5]->ethAddr = strtol(argv[ix+5], NULL, 10);
-        ix += 5;
+        networks[ix]->ip[0] = strtol(argv[ix+1], NULL, 10);
+        networks[ix]->ip[1] = strtol(argv[ix+2], NULL, 10);
+        networks[ix]->bid = strtol(argv[ix+3], NULL, 10);
+        networks[ix]->port = strtol(argv[ix+4], NULL, 10);
+        networks[ix]->ethAddr = strtol(argv[ix+5], NULL, 10);
     }
 
-    readFile();
+    for(int ix = 0; ix < (argc-1)/5; ++ix)
+    {
+        cout << "Network " << (ix+1) << "- \n";
+        cout << "IP: " << networks[ix]->ip[0] << ", " << networks[ix]->ip[1] << endl;
+        cout << "Bridge ID: " << networks[ix]->bid << endl;
+        cout << "Bridge Port: " << networks[ix]->port << endl;
+        cout << "Ethernet Address: " << networks[ix]->ethAddr << endl;
+    }
+
+    //readFile();
     return 0;
 }
 
@@ -60,3 +69,17 @@ void readFile()
     }
 
 }
+/*
+void arp(struct host *newHost, int ip[2])
+{
+    // ARP request format:
+    // ARP REQ target-IP-address source-IP-address source-Ethernet-address
+    string arpRequest = to_string(newHost->ethAddr) + " " + to_string(99);
+    arpRequest = arpRequest + " ARP REQ " + to_string(ip[0]) + " " + to_string(ip[1]) + " " + to_string(newHost->ip[0]) + " " + to_string(newHost->ip[1]) + " " + to_string(newHost->ethAddr);
+    ofstream toFile;
+    string file = "toB";
+    file = file + to_string(newHost->bridge) + "P" + to_string(newHost->bridgePort) + ".txt";
+    toFile.open(file, ios::app);
+    toFile << arpRequest << '\n';
+    toFile.close();
+}*/
